e3cf6f41bef7dc060ff78c8cec8b225639c937a3
WouterJD <47486941+WouterJD@users.noreply.github.com>
2020-05-07 16:19:15 +0200

    Delete FortiusAnt (gui, autostart, simulate) d.bat

--- Opstarten/FortiusAnt (gui, autostart, simulate) d.bat
+++
@@ -1,9 +0,0 @@
-del *.log
-rem No              = 0x00      # 0
-rem Application     = 0x01      # 1
-rem Function        = 0x02      # 2
-rem Data1           = 0x04      # 4			antDongle
-rem Data2           = 0x08      # 8			usbTrainer
-rem Print           = 0x10      # 16
-..\pythoncode\FortiusAnt.py -a -g -n -s -d127 -H 0
-pause
