af44d93f13b657b1cc3d6628a81dd4765e873e70
decodeais <jenspeterschrade@gmail.com>
2021-03-05 19:33:34 +0100

    Update raspberry.py
    You forgot to change the old comment for the IO-Port Initialisations

--- pythoncode/raspberry.py
+++ pythoncode/raspberry.py
@@ -74,26 +74,26 @@ def ShutdownIfRequested():
 # Initialisation of IO-Port's for the LED's
 # ------------------------------------------------------------------------------
 # 
-#       Raspberry Pi Pin  Pin Raspberry Pi
-#    + 3,3 V           1  2   + 5 V
-#  (SDA1) GPI_O2       3  4   + 5 V
-#  (SCL1) GPI_O3       5  6   GND
-#  (GPIO_GCLK) GPI_O4  7  8   GPIO_14 (TXD0)
-#    GND               9  10  GPIO_15 (RXD0)
-# (GPIO_GEN0) GPIO_17 11  12  GPIO_18 (GPIO_GEN1)
-# (GPIO_GEN2) GPIO_27 13  14  GND                    ---> GND
-# (GPIO_GEN3) GPIO_22 15  16  GPIO_23 (GPIO_GEN4)    ---> LED_Dongle
-#    + 3,3 V          17  18  GPIO_24 (GPIO_GEN5)    ---> LED_Cadence
-# (SPI_MISO) GPIO_9   21  22  GPIO_25 (GPIO_GEN6)    ---> LED_Tacx
-# (SPI_SLCK) GPIO_11  23  24  GPIO_8 (SPI_CE0_N)
-#    GND              25  26  GPIO_7 (SPI_CE1_N)
-# (für I2C) ID_SD     27  28  ID_SC (nur für I2C)
-#    GPI_O5           29  30  GND
-#    GPI_O6           31  32  GPIO_12
-#    GPI_O13          33  34  GND
-#    GPI_O19          35  36  GPIO_16
-#    GPIO_26          37  38  GPIO 20
-#    GND              39  40  GPIO 21
+#                      Raspberry Pi Pin  Pin Raspberry Pi
+#                   + 3,3 V           1  2   + 5 V
+#                 (SDA1) GPI_O2       3  4   + 5 V
+#                 (SCL1) GPI_O3       5  6   GND
+#                 (GPIO_GCLK) GPI_O4  7  8   GPIO_14 (TXD0)
+#                   GND               9  10  GPIO_15 (RXD0)
+#                (GPIO_GEN0) GPIO_17 11  12  GPIO_18 (GPIO_GEN1)
+#                (GPIO_GEN2) GPIO_27 13  14  GND                    
+#                (GPIO_GEN3) GPIO_22 15  16  GPIO_23 (GPIO_GEN4)    
+#                   + 3,3 V          17  18  GPIO_24 (GPIO_GEN5)    
+#                (SPI_MISO) GPIO_9   21  22  GPIO_25 (GPIO_GEN6)    
+#                (SPI_SLCK) GPIO_11  23  24  GPIO_8 (SPI_CE0_N)
+#                   GND              25  26  GPIO_7 (SPI_CE1_N)
+#                (für I2C) ID_SD     27  28  ID_SC (nur für I2C)
+#      LedTacx <--  GPI_O5           29  30  GND                   
+#  LedShutdown <--  GPI_O6           31  32  GPIO_12  <-- BtnShutdown
+#   LedCadence <--  GPI_O13          33  34  GND      --> GND
+#       LedBLE <--  GPI_O19          35  36  GPIO_16
+#       LedANT <--  GPIO_26          37  38  GPIO 20
+#                   GND              39  40  GPIO 21
 #
 #
 # Reference https://gpiozero.readthedocs.io/en/stable/api_output.html#led
