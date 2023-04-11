a0acc952ade63f3a0bdac63673d0d1d4c10588d4
Wouter Dubbeldam <47486941+WouterJD@users.noreply.github.com>
2023-04-11 21:22:51 +0200

    Issue#429

--- pythoncode/usbTrainer.py
+++ pythoncode/usbTrainer.py
@@ -551,14 +551,17 @@ class clsTacxTrainer():
         self.DraftingFactor = DraftingFactor
 
     def SetUserConfiguration(self, UserWeight, BicycleWeight, BicycleWheelDiameter, GearRatio):
-        self.BicycleWeight          = BicycleWeight
-        self.BicycleWheelDiameter   = BicycleWheelDiameter
-        self.GearRatio              = GearRatio
-        self.UserWeight             = UserWeight
-        self.UserAndBikeWeight      = UserWeight + BicycleWeight
-        if self.UserAndBikeWeight < 70:
-            logfile.Console("UserAndBikeWeight is set to %d, which is below the expected minimum of 70kg; corrected to 85kg." % self.UserAndBikeWeight)
-            self.UserAndBikeWeight = 75 + 10
+        if debug.on(debug.Function):
+            logfile.Write ("SetUserConfiguration(%s[def=75kg], %s[def=10kg], %s, %s)" % (UserWeight, BicycleWeight, BicycleWheelDiameter, GearRatio))
+
+        if (UserWeight + BicycleWeight) < 70:
+            logfile.Console("The sun of User- and Bike weight is set to %d, which is below the expected minimum of 70kg; it's ignored." % (UserWeight + BicycleWeight))
+        else:
+            self.BicycleWeight          = BicycleWeight
+            self.BicycleWheelDiameter   = BicycleWheelDiameter
+            self.GearRatio              = GearRatio
+            self.UserWeight             = UserWeight
+            self.UserAndBikeWeight      = UserWeight + BicycleWeight
 
     #---------------------------------------------------------------------------
     # SendToTrainer() and ReceivedFromTrainer() to be defined by sub-class
